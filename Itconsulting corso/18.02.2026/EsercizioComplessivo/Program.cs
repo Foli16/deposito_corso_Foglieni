e30db375243f24d69fb5410438f267a9de1b355d
Fabio Foglieni <fabio.foglieni@hotmail.it>
2026-02-18 13:01:26 +0100

    Add files via upload

---
+++ Itconsulting corso/18.02.2026/EsercizioComplessivo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--Registrazione--");
+        Console.Write("Nome utente: ");
+        string usernameR = Console.ReadLine()!;
+        Console.Write("Password: ");
+        string passwordR = Console.ReadLine()!;
+
+        Console.WriteLine("--Login--");
+        string answer;
+        do
+        {
+            Console.Write("Nome utente: ");
+            string usernameL = Console.ReadLine()!;
+            Console.Write("Password: ");
+            string passwordL = Console.ReadLine()!;
+
+            if(usernameL == usernameR && passwordL == passwordR)
+            {
+                Console.WriteLine("Login effettuato!");
+                Console.WriteLine("Vuoi far partire il nostro fantastico conto alla rovescia? (y/n)");
+                string reply = Console.ReadLine()!;
+                if(reply == "y")
+                    Console.WriteLine("Quanto deve essere lungo? Dammi un numero intero");
+                    
+
+                for (int i = 10; i > 0; i--)
+                {
+                    Console.WriteLine($"Conto alla rovescia: {i}");
+                }
+            }
+
+            Console.WriteLine("Vuoi riprovare? (y/n)");
+            answer = Console.ReadLine()!;
+        }while(answer == "y");
+    }
+}
